8669ac4db95c5960522f3ef788186fe193183e8c
Rohit <annamaneni12rohit@gmail.com>
2026-01-10 19:53:19 +0530

    kernels for regression

--- src/linear_regression.cu
+++ src/linear_regression.cu
@@ -3,7 +3,7 @@
 #include <cstdio>
 
 #include "cudaml/models/linear_regression.hpp"
-#include "kernels/linear_regression.cuh"
+#include "kernels/math_kernels.cuh"
 
 namespace cudaml {
     LinearRegression::LinearRegression(int input_features_num, int output_features_num, float lr): Din(input_features_num), Dout(output_features_num), lr(lr), d_w(nullptr){
@@ -23,6 +23,23 @@ namespace cudaml {
         {
             cudaFree(d_w);
         }
+    }
+
+    void LinearRegression::fit(const float* d_X, const float* d_y, int num_samples, int num_iters)
+    {
+        float* d_y_pred = nullptr;
+        float* d_grad = nullptr;
+        int pred_size = num_samples*Dout*sizeof(float);
+        int grad_size = Din*Dout*sizeof(float);
+
+        cudaMalloc(&d_y_pred, pred_size);
+        cudaMalloc(&d_grad, grad_size);
+
+        for (int T = 0; T < num_iters; T++)
+        {
+            //Call kernels and copy back to MM
+        }
+        //free cuda mem
     }
 }
 
