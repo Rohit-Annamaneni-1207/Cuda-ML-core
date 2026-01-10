4557eb3e024a0fd8adf9a939c44ab3034658dc94
rohit <rohit@LAPTOP-G7UJ973S.localdomain>
2026-01-11 00:14:30 +0530

    Fix .gitignore and remove tracked ignored files

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/rohit/projects/Cuda-ML-core
-# Build directory: /home/rohit/projects/Cuda-ML-core/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(LinearRegressionTest "/home/rohit/projects/Cuda-ML-core/build/test_linear_regression")
-set_tests_properties(LinearRegressionTest PROPERTIES  _BACKTRACE_TRIPLES "/home/rohit/projects/Cuda-ML-core/CMakeLists.txt;48;add_test;/home/rohit/projects/Cuda-ML-core/CMakeLists.txt;0;")
